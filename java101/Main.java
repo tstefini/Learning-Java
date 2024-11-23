7cd36987a96f7e2646376c9286fd508d7cfe9178
tstefini <stefinit@nycstudents.net>
2024-11-22 21:06:22 -0500

    updates

--- java101/Main.java
+++ java101/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("why")
+public class Main{
+	static double f(double x) {
+			return 2 * x;
 	}
-}
+	public static void main(String[] args) {
+		double x = 3;
+
+		;
+				System.out.println("f(3): " + f(x));
+		}
+	
+	}
